7a2d4738eea756e052b2ade79bcab43a9d8548ff
4halavandala <1terrormachine1@gmail.com>
2023-02-01 13:16:41 +0300

    fix db connect in docker, fix queues

--- producer/pkg/handler/handler.go
+++ producer/pkg/handler/handler.go
@@ -8,7 +8,7 @@ func SetupRouters(app *gin.Engine) {
 	v1 := app.Group("/v1")
 	{
 		v1.GET("/ping", Ping)
-		v1.POST("/publish", SendMessageInQ)
+		v1.POST("/publish/example", SendMessageInQ)
 	}
 
 }
