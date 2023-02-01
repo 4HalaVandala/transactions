62e512d4071f31b526af5e3f54e390fd83baded8
4halavandala <1terrormachine1@gmail.com>
2023-02-01 13:21:48 +0300

    fix docker db connect, queues v2

--- schema/01_init_database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TYPE txType AS ENUM ('deposit', 'withdrawal')
-
-CREATE TABLE transactions (
- id uint PRIMARY KEY
- tx_Type txType
- amount uint
- created_at timestamp without time zone
- updated_at timestamp without time zone
-);
